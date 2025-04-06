fb24fc181a639aea499e36fe2f4e13d4235b0002
Vitor Martins <vitorrhenriquem@gmail.com>
2025-04-06 17:20:47 -0400

    Encerramento da aula

---
+++ exemplo_lista.py
@@ -0,0 +1,27 @@
+#adicionando produtos na lista
+
+nome1: list = "vitor"
+
+nome2: list = "geovanna"
+
+nome3: list = "Bianca"
+
+
+nomes: list = []
+
+nomes.append(nome1)
+
+nomes.append(nome2)
+
+nomes.append(nome3)
+
+#removendo ultimo produto da lista
+nomes.pop()
+
+
+#removendo produto especifico
+
+nomes.remove(nome1)
+print(nomes)
+
+
